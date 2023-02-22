e3fc824f4265973ac6e03d252a1880df9b42f57d
Yago Carro <ycarro@c1r1s1.42madrid.com>
2023-02-22 14:06:08 +0100

    add_angle_class

---
+++ include/angle.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   angle.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycarro <ycarro@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 13:57:17 by ycarro            #+#    #+#             */
+/*   Updated: 2023/02/22 14:05:01 by ycarro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANGLE_H
+# define ANGLE_H
+
+# include "libft.h"
+# include "error_code.h"
+# include "direction.h"
+
+typedef struct s_angle
+{
+	double	radians;
+}				t_angle;
+
+#endif
