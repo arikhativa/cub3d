443f2353908fdafe5110368ccb6b47d3d3e166c2
Moon Byte <moon_byte@protonmail.com>
2023-03-19 13:04:54 +0100

    changed 100 unit to 1 unit

--- src/collision/collision.c
+++ src/collision/collision.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   collision.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ycarro <ycarro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:59:18 by ycarro            #+#    #+#             */
-/*   Updated: 2023/02/28 12:38:51 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/03/19 13:03:54 by ycarro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,12 @@ t_collision	check_in_range(t_ray *ray_data, char **map)
 {
 	t_point	collider;
 
-	collider.x = ray_data->x_pos.x / 100;
-	collider.y = ray_data->x_pos.y / 100;
+	collider.x = ray_data->x_pos.x;
+	collider.y = ray_data->x_pos.y;
 	if (map[collider.x][collider.y] == WALL_CHAR)
 		return (X_COLLISION);
-	collider.x = ray_data->y_pos.x / 100;
-	collider.y = ray_data->y_pos.y / 100;
+	collider.x = ray_data->y_pos.x;
+	collider.y = ray_data->y_pos.y;
 	if (map[collider.x][collider.y] == WALL_CHAR)
 		return (Y_COLLISION);
 	return (NO_COLLISION);
