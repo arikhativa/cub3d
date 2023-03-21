abe05abf6aba313b87ddecdd232a1793d45bcf9f
Arik Hativa <arikhativa@gmail.com>
2023-03-21 11:24:55 +0200

    FloatPoint

---
+++ unit_test/fpoint/fpoint.t.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utest.t.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 14:21:17 by yoav              #+#    #+#             */
+/*   Updated: 2023/02/14 12:27:39 by yrabby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fpoint.h"
+#include "unit_test.h"
+
+void	test_fpoint(void)
+{
+	t_fpoint	p1;
+	t_fpoint	p2;
+
+	p1 = fpoint_init(1.1, 2.2);
+	p2 = fpoint_copy(p1);
+	CU_ASSERT_EQUAL(p1.x, 1.1);
+	CU_ASSERT_EQUAL(p1.y, 2.2);
+	CU_ASSERT_EQUAL(p1.x, p2.x);
+	CU_ASSERT_EQUAL(p1.y, p2.y);
+}
+
+void	test_fpoint_init(void)
+{
+	t_fpoint		p;
+
+	p = fpoint_init(5.5, 6.5);
+	CU_ASSERT_EQUAL(5.5, p.x);
+	CU_ASSERT_EQUAL(6.5, p.y);
+}
+
+void	test_fpoint_to_point(void)
+{
+	t_fpoint		p;
+	t_point			p2;
+
+	p = fpoint_init(5.5, 6.5);
+	p2 = fpoint_to_point(p);
+	CU_ASSERT_EQUAL(5, p2.x);
+	CU_ASSERT_EQUAL(6, p2.y);	
+}
+
+void	test_point_to_fpoint(void)
+{
+	t_fpoint		p;
+	t_point			p2;
+
+	p2 = point_init(5, 6);
+	p = point_to_fpoint(p2);
+	CU_ASSERT_EQUAL(5.0, p.x);
+	CU_ASSERT_EQUAL(6.0, p.y);	
+}
