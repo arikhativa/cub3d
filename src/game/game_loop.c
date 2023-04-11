3e2699e7797c9b4e7b9f249882e38050981b2050
Moon Byte <moon_byte@protonmail.com>
2023-04-11 23:34:55 +0200

    exit_esc_key

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   class.c                                            :+:      :+:    :+:   */
+/*   game_loop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ycarro <ycarro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:00:59 by yrabby            #+#    #+#             */
-/*   Updated: 2023/02/15 13:55:57 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/04/11 23:32:18 by ycarro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	key_press(int keycode, t_game *g)
 	if (mlx_keys_is_movement(keycode))
 		game_handler_movement(g, keycode);
 	if (mlx_keys_is_exit(keycode))
-		game_handler_exit(g, keycode);
+		game_handler_exit(g);
 	game_draw(g);
 	return (SUCCESS);
 }
