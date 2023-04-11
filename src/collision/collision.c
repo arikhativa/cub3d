3cfb957c61c939e74de022d2dd68e38ddabb53aa
Yago Carro <ycarro@c1r1s1.42madrid.com>
2023-04-11 15:49:31 +0200

    dda_fix_collider

--- src/collision/collision.c
+++ src/collision/collision.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   collision.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ycarro <ycarro@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ycarro <ycarro@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:59:18 by ycarro            #+#    #+#             */
-/*   Updated: 2023/04/08 16:52:07 by ycarro           ###   ########.fr       */
+/*   Updated: 2023/04/11 15:44:58 by ycarro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,15 @@ int	is_collides(t_ray *ray_data, char **map, t_collinfo *collinfo)
 t_collision	check_in_range(t_ray *ray_data, char **map, t_collinfo *collinfo)
 {
 	collinfo->collider = fpoint_to_point(ray_data->x_pos);
+	if (!(fpoint_equal(ray_data->start, ray_data->x_pos)) && \
+		ray_data->incrementor.x == -1)
+		collinfo->collider.x--;
 	if (map[collinfo->collider.y][collinfo->collider.x] == WALL_CHAR)
 		return (X_COLLISION);
 	collinfo->collider = fpoint_to_point(ray_data->y_pos);
+	if (!(fpoint_equal(ray_data->start, ray_data->y_pos)) && \
+		ray_data->incrementor.y == -1)
+		collinfo->collider.y--;
 	if (map[collinfo->collider.y][collinfo->collider.x] == WALL_CHAR)
 		return (Y_COLLISION);
 	return (NO_COLLISION);
