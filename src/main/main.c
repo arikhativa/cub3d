5ecfbfb16c13855ba084f9d20153cf21c0c4a278
Yago Carro <ycarro@c1r1s1.42madrid.com>
2023-04-19 16:06:57 +0200

    main_fix_load_exit

--- src/main/main.c
+++ src/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ycarro <ycarro@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 09:50:39 by al7aro            #+#    #+#             */
-/*   Updated: 2023/02/16 11:19:44 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/04/19 16:04:54 by ycarro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,15 @@ int	main(int argc, char **argv)
 		{
 			err = game_init(game);
 			if (SUCCESS == err)
+			{
 				err = game_load(game, argv[1]);
+				if (SUCCESS != err)
+					exit (error_code_print_on_exit(err));
+			}
 			if (SUCCESS == err)
 				err = game_start(game);
-			game_destroy(&game);
 		}
+		game_destroy(&game);
 	}
 	return (error_code_print_on_exit(err));
 }
